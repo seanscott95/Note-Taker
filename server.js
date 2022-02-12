b2ece5861b6c4dc9f7f3ae93f8923b7a90fe7b58
Sean <seanms418@gmail.com>
2022-02-12 19:12:22 +1100

    added process.env.PORT to port variable

--- server.js
+++ server.js
@@ -4,42 +4,35 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
 const db = require('./db/db.json');
-const PORT = 3001;
-
+const PORT = process.env.PORT || 3001;
 // Initialised app varibale by setting it to the value of express
 const app = express();
 
 // Middleware used to set up the express app to handle data parsing 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serves css and js files from the public directory
 app.use(express.static('public'));
 
-// GET wildcard ROUTE, serves when the requested resource doesnt exist 
-app.get("*", (req, res) => {
-    console.info(`New ${req.method} request received`)
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
 
 // GET Route for notes.html page
-app.get('/notes', (req, res) => {
-    console.info(`New ${req.method} request received`)
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) => {
+    console.info(`New ${req.method} request received for /notes`)
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-
-app.get('/api/notes', (req, res) => {
-    console.info(`New ${req.method} request received`)
+app.get("/api/notes", (req, res) => {
+    console.info(`New ${req.method} request received for /api/notes`)
     res.json(db)
 });
 
+
 app.post('/api/notes', (req, res) => {
-    console.info(`New ${req.method} request received`)
-    console.info(req.rawHeaders);
+    console.info(`New ${req.method} request received for /api/notes`)
     // Adding request body data to variables
     const { title, text } = req.body;
-
+    
     // If all the required properties are present
     if (title && text) {
         // New variable for the object to be saved
@@ -49,14 +42,14 @@ app.post('/api/notes', (req, res) => {
             note_id: uuid(),
         }
 
-        fs.readFile("/db/db.json", "utf-8", (err, data) => {
+        fs.readFile("./db/db.json", "utf-8", (err, data) => {
             if (err) {
                 console.info(err);
             } else {
                 const parsedNotes = JSON.parse(data);
                 parsedNotes.push(newNote);
 
-                fs.writeFile("/db/db.json", JSON.stringify(parsedNotes), (err) =>
+                fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (err) =>
                     err ? console.error(err) : console.log('Success!')
                 )
             }
@@ -66,7 +59,7 @@ app.post('/api/notes', (req, res) => {
             status: 'success',
             body: newNote,
         }
-
+        console.info(response);
         res.status(201).json(response);
     } else {
         res.status(500).json('Error in posting note');
@@ -77,10 +70,37 @@ app.delete("/api/notes/:id", (req, res) => {
     // should receive a query parameter that contains the id of a note to delete. To delete a note, 
     // you'll need to read all notes from the `db.json` file, remove the note with the 
     // given `id` property, and then rewrite the notes to the `db.json` file
+
     console.info(`New ${req.method} request received for id ${req.params.id} note`);
 
     const filteredNotes = [];
-    
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            console.info(err);
+        } else {
+            const { id } = req.params;
+
+            for (let i = 0; i < db.length; i++) {
+                const index = db[i];
+                if (id === index.note_id) {
+                    filteredNotes = db.filter(note => note.note_id != id);
+
+                    fs.writeFile("./db/db.json", JSON.stringify(filteredNotes), (err) =>
+                        err ? console.error(err) : console.log('Success!')
+                    );
+                    return res.json("Note ${id} deleted succefully!")
+                }
+            }
+            return res.json("Failed to find note with that id.")
+        }
+    })
+});
+
+// GET wildcard ROUTE, serves when the requested resource doesnt exist 
+app.get("*", (req, res) => {
+    console.info(`New ${req.method} request received for wildcard`)
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 // Listens on port 3001 for connections
