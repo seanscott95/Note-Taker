43efe2913b9eeaab516a79293b9dcf8e30b1f2ab
Sean <seanms418@gmail.com>
2022-02-12 16:55:32 +1100

    added delete request for api/notes/:id

--- server.js
+++ server.js
@@ -73,6 +73,16 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+    // should receive a query parameter that contains the id of a note to delete. To delete a note, 
+    // you'll need to read all notes from the `db.json` file, remove the note with the 
+    // given `id` property, and then rewrite the notes to the `db.json` file
+    console.info(`New ${req.method} request received for id ${req.params.id} note`);
+
+    const filteredNotes = [];
+    
+});
+
 // Listens on port 3001 for connections
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
