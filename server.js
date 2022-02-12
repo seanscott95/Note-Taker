d98db7076ee106e52f03d6092b4b6caa4b7b9f9d
Sean <seanms418@gmail.com>
2022-02-12 14:09:38 +1100

    added middleware to server.js

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+// Imports express package and database json file
 const express = require('express');
 const db = require('./db/db.json');
 const PORT = 3001;
 
+
+// Initialised app varibale by setting it to the value of express
 const app = express();
+
+// Middleware used to set up the express app to handle data parsing 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serves css and js files from the public directory
+app.use(express.static('public'));
