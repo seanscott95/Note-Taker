4fb258b08465cbaaf4c4ed7f2bf9a09e919b06e0
Sean <seanms418@gmail.com>
2022-02-12 14:21:36 +1100

    added html get wildcard route and /notes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const db = require('./db/db.json');
 const PORT = 3001;
 
-
 // Initialised app varibale by setting it to the value of express
 const app = express();
 
@@ -14,12 +13,20 @@ app.use(express.json());
 // Serves css and js files from the public directory
 app.use(express.static('public'));
 
+// GET wildcard ROUTE, serves when the requested resource doesnt exist 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
+// GET Route for notes.html page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 
 
 // Listens on port 3001 for connections
 app.listen(PORT, () =>
-  console.log(`Serving static asset routes on port ${PORT}!`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
